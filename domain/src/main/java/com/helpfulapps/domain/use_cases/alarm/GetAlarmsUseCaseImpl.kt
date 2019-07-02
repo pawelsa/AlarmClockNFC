224141b5ff09f9c87b105a37470b3c43c75f602e
pawelsa <pawel.sapek97@gmail.com>
2019-07-02 23:37:46 +0200

    Updated todo

--- domain/src/main/java/com/helpfulapps/domain/use_cases/alarm/GetAlarmsUseCaseImpl.kt
+++ domain/src/main/java/com/helpfulapps/domain/use_cases/alarm/GetAlarmsUseCaseImpl.kt
@@ -6,20 +6,7 @@ import com.helpfulapps.domain.repository.WeatherRepository
 import com.helpfulapps.domain.use_cases.alarm.definition.GetAlarmsUseCase
 import io.reactivex.Single
 
-// todo IMPORTANT !!
-/**
- * podzielić przychodzącą prognozę pogody na dni
- *
- * dla każdej godziny przeanalizować pogodę - temperatura, opady deszczy/śniegu
- *
- * zwracany obiekt powinien zawierać informację dla dnia i godzin
- *
- * alarm powinien wyświetlać infotmację dla całego dnia jako ważniejsze niż godzinowa - priority
- *
- *
- * */
-
-
+//TODO write it complete - it should get the most important data
 class GetAlarmsUseCaseImpl(
     private val alarmRepository: AlarmRepository,
     private val weatherRepository: WeatherRepository
