180677560d81ec20d622e4ae8f5bd58c9ed0e8a6
pawelsa <pawel.sapek97@gmail.com>
2019-10-28 23:47:58 +0100

    reverted changes in switch useCase

--- domain/src/main/java/com/helpfulapps/domain/use_cases/alarm/SwitchAlarmUseCaseImpl.kt
+++ domain/src/main/java/com/helpfulapps/domain/use_cases/alarm/SwitchAlarmUseCaseImpl.kt
@@ -1,7 +1,6 @@
 package com.helpfulapps.domain.use_cases.alarm
 
 import com.helpfulapps.domain.repository.AlarmClockManager
-import com.helpfulapps.domain.repository.AlarmRepository
 import com.helpfulapps.domain.use_cases.type.CompletableUseCaseWithParameter
 import io.reactivex.Completable
 
@@ -10,13 +9,10 @@ interface SwitchAlarmUseCase : CompletableUseCaseWithParameter<SwitchAlarmUseCas
     data class Params(val alarmId: Long)
 }
 
-class SwitchAlarmUseCaseImpl(
-    private val _repository: AlarmRepository,
-    private val _clockManager: AlarmClockManager
-) : SwitchAlarmUseCase {
+class SwitchAlarmUseCaseImpl(private val _repository: AlarmClockManager) : SwitchAlarmUseCase {
 
     //todo it should change value in the alarm to off or on and then interact with alarm manager
     override fun invoke(parameter: SwitchAlarmUseCase.Params): Completable {
-        return _repository.switchAlarm(parameter.alarmId)
+        return _repository.stopAlarm(parameter.alarmId)
     }
 }
