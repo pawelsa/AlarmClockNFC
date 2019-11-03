f8a486cf24dbdd13db4a9c8ca249399471951401
pawelsa <pawel.sapek97@gmail.com>
2019-11-03 22:30:29 +0100

    removing alarms works

--- domain/src/main/java/com/helpfulapps/domain/use_cases/alarm/RemoveAlarmUseCaseImpl.kt
+++ domain/src/main/java/com/helpfulapps/domain/use_cases/alarm/RemoveAlarmUseCaseImpl.kt
@@ -1,5 +1,6 @@
 package com.helpfulapps.domain.use_cases.alarm
 
+import com.helpfulapps.domain.repository.AlarmClockManager
 import com.helpfulapps.domain.repository.AlarmRepository
 import com.helpfulapps.domain.use_cases.type.CompletableUseCaseWithParameter
 import io.reactivex.Completable
@@ -9,8 +10,12 @@ interface RemoveAlarmUseCase : CompletableUseCaseWithParameter<RemoveAlarmUseCas
     data class Params(val alarmId: Long)
 }
 
-class RemoveAlarmUseCaseImpl(private val _repository: AlarmRepository) : RemoveAlarmUseCase {
+class RemoveAlarmUseCaseImpl(
+    private val _clockManager: AlarmClockManager,
+    private val _repository: AlarmRepository
+) : RemoveAlarmUseCase {
 
     override fun invoke(parameter: RemoveAlarmUseCase.Params): Completable =
-        _repository.removeAlarm(parameter.alarmId)
+        _clockManager.stopAlarm(parameter.alarmId)
+            .concatWith(_repository.removeAlarm(parameter.alarmId))
 }
