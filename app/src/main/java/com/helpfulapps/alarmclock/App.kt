83b4772982de18af10ce9486acf602b2320fd530
psapek <psapek@infotower.pl>
2019-11-23 23:27:26 +0100

    downloading forecast works

--- app/src/main/java/com/helpfulapps/alarmclock/App.kt
+++ app/src/main/java/com/helpfulapps/alarmclock/App.kt
@@ -2,11 +2,14 @@ package com.helpfulapps.alarmclock
 
 import android.app.Application
 import android.nfc.NfcAdapter
+import androidx.work.*
 import com.helpfulapps.alarmclock.di.Modules
 import com.helpfulapps.alarmclock.helpers.Settings
+import com.helpfulapps.alarmclock.worker.DownloadWeatherWorker
 import org.koin.android.ext.android.inject
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
+import java.util.concurrent.TimeUnit
 
 class App : Application() {
 
@@ -17,11 +20,36 @@ class App : Application() {
         startKoin {
             androidContext(this@App).modules(Modules.modules)
         }
+
+        if (settings.city != "-1") {
+            periodicWeatherDownload()
+        }
+
         if (settings.firstRun) {
             val nfcAdapter = NfcAdapter.getDefaultAdapter(this)
             settings.hasNfc = nfcAdapter != null
             settings.firstRun = false
         }
 
+    }
+
+    private fun periodicWeatherDownload() {
+        val downloadConstraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+
+        val downloadWeather =
+            PeriodicWorkRequestBuilder<DownloadWeatherWorker>(24, TimeUnit.HOURS)
+                .setConstraints(downloadConstraints)
+                .build()
+        WorkManager.getInstance(this).enqueueUniquePeriodicWork(
+            FORECAST_DOWNLOAD_WORK,
+            ExistingPeriodicWorkPolicy.REPLACE,
+            downloadWeather
+        )
+    }
+
+    companion object {
+        const val FORECAST_DOWNLOAD_WORK = "com.helpfulapps.alarmclock.download_forecast_work"
     }
 }
