495d67009585f6c25d1fcaaedbcac0c786f36174
pawelsa <pawel.sapek97@gmail.com>
2019-12-17 21:10:50 +0100

    refactor

--- data/src/main/java/com/helpfulapps/data/db/weather/dao/WeatherDao.kt
+++ data/src/main/java/com/helpfulapps/data/db/weather/dao/WeatherDao.kt
@@ -6,10 +6,7 @@ import io.reactivex.Single
 
 interface WeatherDao {
     fun insert(dayWeatherData: DayWeatherData): Single<Boolean>
-    /*    fun update(dayWeatherData: DayWeatherData):Single<Boolean>
-        fun delete(dayWeatherData: DayWeatherData):Single<Boolean>*/
     fun clearWeatherTables()
-
     fun streamWeatherList(): Flowable<DayWeatherData>
     fun getWeatherForTime(time: Long): Single<DayWeatherData>
 }
