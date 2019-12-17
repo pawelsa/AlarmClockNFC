479d82e7e1820b53cc94d81bafa610935a84a3e6
pawelsa <pawel.sapek97@gmail.com>
2019-12-17 20:59:43 +0100

    should work

--- lib/src/main/java/com/helpfulapps/lib/db/stats/dao/StatsDao.kt
+++
@@ -1,9 +0,0 @@
-package com.helpfulapps.data.db.stats.dao
-
-import com.helpfulapps.data.db.stats.model.AlarmStatsData
-import io.reactivex.Single
-
-interface StatsDao {
-    fun save(alarmStats: AlarmStatsData): Single<Boolean>
-    fun getAll(): Single<List<AlarmStatsData>>
-}
